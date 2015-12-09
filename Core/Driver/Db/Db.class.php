f534c55b8e7f425662e2902a9eef19fb65cfe8fd
宇润 <admin@yurunsoft.com>
2015-12-09 10:37:29 +0800

    添加注释

--- Core/Driver/Db/Db.class.php
+++ Core/Driver/Db/Db.class.php
@@ -5,8 +5,20 @@
  */
 class Db extends Driver
 {
+	/**
+	 * 返回操作是否执行成功
+	 * @var int
+	 */
 	const RETURN_ISOK = 0;
+	/**
+	 * 返回语句影响行数
+	 * @var int
+	 */
 	const RETURN_ROWS = 1;
+	/**
+	 * 返回最后插入的自增ID
+	 * @var int
+	 */
 	const RETURN_INSERT_ID = 2;
 }
 Db::init();
