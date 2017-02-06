8a99899ea4ee4af87474f12fe7b558408332c179
宇润 <admin@yurunsoft.com>
2017-02-06 09:57:54 +0800

    提交一下demo

--- Demo/Module/Home/Control/IndexControl.class.php
+++ Demo/Module/Home/Control/IndexControl.class.php
@@ -3,6 +3,10 @@ class IndexControl extends Control
 {
 	public function index()
 	{
+		// 驱动使用
+		// Cache::set('memcache.yurunphp',Yurun::YURUN_VERSION);
+		// echo Cache::get('memcache.yurunphp');
+		
 		$model = new TestModel;
 		$this->view->msg = $model->getMsg();
 		$this->view->memory = memory_get_usage() / 1024;
